c045d9904122a6d06f2d99e354ec68b414abe91f
ruflun <enzohochin@gmail.com>
2026-07-18 12:36:55 +0200

    Initial commit

---
+++ src/client/java/io/github/ruflun/client/RuflunSThingamabobsDataGenerator.java
@@ -0,0 +1,11 @@
+package io.github.ruflun.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RuflunSThingamabobsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}

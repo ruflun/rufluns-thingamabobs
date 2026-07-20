0954c3d1d5e7852a606ab918900b41f869993268
ruflun <enzohochin@gmail.com>
2026-07-20 16:12:16 +0200

    Removed static tags in favor of datagenerated tags

--- src/client/java/io/github/ruflun/client/datagen/RuflunSThingamabobsBlockTagProvider.java
+++ src/client/java/io/github/ruflun/client/datagen/RuflunSThingamabobsBlockTagProvider.java
@@ -1,9 +1,10 @@
 package io.github.ruflun.client.datagen;
 
+import io.github.ruflun.tags.ModTags;
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagsProvider;
 import net.minecraft.core.HolderLookup;
-import net.minecraft.tags.BlockTags;
+import net.minecraft.resources.Identifier;
 import org.jspecify.annotations.NonNull;
 
 import java.util.concurrent.CompletableFuture;
@@ -14,9 +15,24 @@ public class RuflunSThingamabobsBlockTagProvider extends FabricTagsProvider.Bloc
         super(output, registryLookupFuture);
     }
 
+    // Shorthand method
+    private static Identifier id(String id) { return Identifier.parse(id); }
+
     @Override
     protected void addTags(HolderLookup.@NonNull Provider registries) {
+        //=== MOD TAGS ===//
 
+        // Blocks that can be used to open Uper Cores
+        getOrCreateRawBuilder(ModTags.Blocks.UPERCORE_OPENER)
+                .addElement(id("minecraft:bedrock"))
+                .addElement(id("minecraft:obsidian"))
+                .addElement(id("minecraft:crying_obsidian"))
+                .addElement(id("minecraft:diamond_block"))
+                .addElement(id("minecraft:netherite_block"));
+
+
+
+        //=== VANILLA TAGS ===//
     }
 
     @Override
--- src/client/java/io/github/ruflun/client/datagen/RuflunSThingamabobsItemTagProvider.java
+++ src/client/java/io/github/ruflun/client/datagen/RuflunSThingamabobsItemTagProvider.java
@@ -1,8 +1,10 @@
 package io.github.ruflun.client.datagen;
 
+import io.github.ruflun.tags.ModTags;
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagsProvider;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.resources.Identifier;
 import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
 
@@ -14,13 +16,30 @@ public class RuflunSThingamabobsItemTagProvider extends FabricTagsProvider.ItemT
         super(output, registryLookupFuture, blockTagsProvider);
     }
 
+    // Shorthand method
+    private static Identifier id(String id) { return Identifier.parse(id); }
+
     public RuflunSThingamabobsItemTagProvider(FabricPackOutput output, CompletableFuture<HolderLookup.Provider> registryLookupFuture) {
         super(output, registryLookupFuture);
     }
 
     @Override
     protected void addTags(HolderLookup.Provider registries) {
+        //=== MOD TAGS ===///
 
+        // Essence items
+        getOrCreateRawBuilder(ModTags.Items.ESSENCES)
+                .addElement(id("rufrix:uper_essence"));
+
+        // Floating items
+        getOrCreateRawBuilder(ModTags.Items.FLOATING)
+                .addTag(id("rufrix:essences"))
+                .addElement(id("minecraft:breeze_rod"))
+                .addElement(id("minecraft:wind_charge"));
+
+
+
+        //=== VANILLA TAGS ===//
     }
 
     @Override
